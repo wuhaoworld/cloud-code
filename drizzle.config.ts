6479397f1d90869b0e98fc06cec5b4d415ff30a7
hiratwu <ratwu@qq.com>
2026-06-20 10:45:47 +0800

    feat: add auth schema and update database configuration

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'drizzle-kit';
 
 export default defineConfig({
   out: './drizzle',
-  schema: './db/schema.ts',
+  schema: ['./db/schema.ts', './db/auth-schema.ts'],
   dialect: 'sqlite',
   dbCredentials: {
     url: process.env.DB_FILE_NAME!,
